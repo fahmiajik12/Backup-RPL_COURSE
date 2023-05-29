5bb8d8212fcd30ffe187db663aa56e3aa8c0659c
Fahmi <fahmiaji123.fak@gmail.com>
2023-05-29 15:37:20 +0700

    v1

---
+++ application/views/kontak.php
@@ -0,0 +1,48 @@
+<!--================Home Banner Area =================-->
+<section class="banner_area">
+    <div class="banner_inner d-flex align-items-center">
+        <div class="kontak bg-parallax" data-stellar-ratio="0.9" data-stellar-vertical-offset="0" data-background="">
+        </div>
+        <div class="container">
+            <div class="banner_content text-center">
+                <h2 data-aos="fade-up" data-aos-duration="1600">Kontak</h2>
+                <div data-aos="fade-up" data-aos-duration="1600" class="page_link">
+                    <a href="<?= base_url('welcome') ?>">Beranda</a>
+                    <a href="<?= base_url('welcome/kontak') ?>">Kontak</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--================End Home Banner Area =================-->
+
+<!--================Contact Area =================-->
+<section class="contact_area p_40">
+    <div class="container">
+        <div style="width: 100%"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3955.4858517815715!2d110.22449021115777!3d-7.521864474194857!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a8f593d665a8f%3A0xd4e74aab2b530e83!2sKampus%202%20Universitas%20Muhammadiyah%20Magelang!5e0!3m2!1sid!2sid!4v1685339549562!5m2!1sid!2sid" width="1200" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div><br />
+        <div class="row">
+            <div class="col-lg-9">
+                <div class="contact_info">
+                    <div class="info_item">
+                        <i class="lnr lnr-home"></i>
+                        <h6>Jl.MAGELANG
+                            KABUPATEN MAGELANG, JAWA TENGAH</h6>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                    <div class="info_item">
+                        <i class="lnr lnr-phone-handset"></i>
+                        <h6><a href="#">
+                                08888888888</a></h6>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                    <div class="info_item">
+                        <i class="lnr lnr-envelope"></i>
+                        <h6><a href="#">apps.Socamp@gmail.com</a></h6>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--================Contact Area =================-->
